d1e7d0e212d05acce77191d68466f91bcd213a2b
ananiazania <ananiazania@gmail.com>
2018-07-11 05:56:21 +0700

    menambahkan view update gunung sukses

---
+++ application/views/Gunung/admin/Update_Sukses.php
@@ -0,0 +1,37 @@
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="FaberNainggolan">
+    <title>CodeIgniter dengan Bootstrap</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="<?=base_url()?>assets/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Bootstrap bootstrap-dateptimeicker.css-->
+    <link href="<base_url();?>assets/css/bootstrap-datetimepicker.min.css" rel="stylesheet" media="screen">
+
+
+    <!-- Bootstrap bootstrap-datepticker.css-->
+    <link href="<base_url();?>assets/css/bootstrap-datepicker.min.css" rel="stylesheet" media="screen">
+
+
+    </head>
+
+  <body>
+
+<div class="alert alert-success">
+<strong> Data berhasil di Update</strong>
+<?php
+	echo anchor('Gunung/index', 'Lihat data makana');
+  ?>
+</div>
+
+<script src="<?=base_url()?>assets/js/jquery.js"></script>
+    <script src="<?=base_url()?>assets/js/bootstrap.min.js"></script>
+
+
+  </body>
+</html>
